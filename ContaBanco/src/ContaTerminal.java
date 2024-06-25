06e1e3f76e7c889972e7138bb6d1a69b07cb2d41
Lor3dev <lopaiva05@gmail.com>
2024-06-25 04:38:57 -0300

    ProcessoSeletivo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,31 +1,18 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    String nomeCliente, agencia;
-    int numero;
-    double saldo;
+   
+    public static void main(String[] args) {
+	
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+                continue;
+            
+            System.out.println(numero);
+            
+        }
+        //Qual a saída no console ?
+    
+        }
+    }
 
-public static void main(String[]args) {
-
-Scanner entradadeDados = new Scanner (System.in);
-
-System.out.print("Digite seu nome:");
-String nomeCliente = entradadeDados.nextLine();
-
-System.out.print("Digite o numero da agência:");
-String agencia = entradadeDados.nextLine();
-
-System.out.print("Digite o numero da conta:");
-int numero = entradadeDados.nextInt();
-
-System.out.print("Digite o saldo da conta:");
-double saldo = entradadeDados.nextDouble();
-
-System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente,agencia, numero, saldo );
-
-
-}
-
-
-
-}
