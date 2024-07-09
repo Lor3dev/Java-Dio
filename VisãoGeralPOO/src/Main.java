eec05f94e97977df5fc22bbfba046fae9f93f03c
Lor3dev <162937610+Lor3dev@users.noreply.github.com>
2024-07-08 22:11:37 -0300

    Add files via upload

---
+++ VisãoGeralPOO/src/Main.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+import Dominio.Curso;
+import Dominio.Mentoria;
+
+public class Main {
+
+    public static void main(String[]args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso python");
+        curso2.setDescricao("descrição curso python");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setDescricao("Mentoria para curso java");
+        mentoria1.setData(LocalDate.now());
+        
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
+    } 
+
+}
